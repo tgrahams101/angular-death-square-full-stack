48aea77f3471a24b7861d276783aff610fe2e35c
Ted Anyansi <tanyansi@gmail.com>
2018-05-11 02:23:02 -0700

    added routing to info/:id path. when a turret is clicked, it has a Routerlink to a 'info/:id' path and when that path matched with the display of an InfoComponent which makes a API call to get turret damage stats, which is saved as a component property. Portions of this property is rendered on page via data binding between view and component property

--- imperial-starfleet/src/app/info-window/info-window.component.ts
+++ imperial-starfleet/src/app/info-window/info-window.component.ts
@@ -1,4 +1,8 @@
+import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-info-window',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-window.component.css']
 })
 export class InfoWindowComponent implements OnInit {
-
-  constructor() { }
+  dataBanks: any;
+  constructor(private http: Http, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach( (element) => {
+        console.log('NG ON IT', element.id)
+        if (element.id) {
+          this.findTurret(element.id);
+        }
+    });
+  }
+
+  findTurret(turretNumber) {
+    console.log('called findturrett method');
+    this.http.get('http://localhost:3000/api/turret/' + turretNumber)
+    .toPromise()
+    .then( (response) => {
+      console.log(response);
+      console.log(response.json());
+      this.dataBanks = response.json();
+    });
   }
 
 }
